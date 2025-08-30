bedb9237264fc6810b5046dc1e32b37d09b4a866
bracesproul <braceasproul@gmail.com>
2025-08-29 17:17:05 -0700

    chore: Remove logs in restart run route

--- apps/web/src/app/api/restart-run/route.ts
+++ apps/web/src/app/api/restart-run/route.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { v4 as uuidv4 } from "uuid";
 import {
   GITHUB_TOKEN_COOKIE,
@@ -70,13 +69,6 @@ async function createNewSession(
 ): Promise<AgentSession> {
   const newThreadId = uuidv4();
   const hasNext = inputs.threadState.next.length > 0;
-
-  console.log("\n\nCONFIG");
-  console.dir(getCustomConfigurableFields(inputs.threadConfig), {
-    depth: null,
-  });
-  console.log("\n\nCONFIGURABLE");
-  console.dir(inputs.threadConfig.configurable, { depth: null });
 
   const run = await client.runs.create(newThreadId, inputs.graphId, {
     command: {
